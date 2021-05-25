2496f2360002a2a8b56038ef1bb2f6b010ba8c90
BostjanStupica <bostjan@stupica.com>
2021-05-25 13:31:45 +0200

    Build update.

---
+++ copy2local.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This ..
+#
+
+scp target/httpsTest-1.0.0.jar pc-mac:/home/bostjans/bin/java/
+scp target/httpsTest-1.0.0.jar astronomix:/home/stupicab/bin/java/
+
+echo That_s it.
+
+exit 0
--- run01.sh
+++ run01.sh
@@ -6,12 +6,20 @@
 PROG_DIR=.
 PROG_DIR=target
 
-java -jar $PROG_DIR/httpsTest-1.0.0-jar-with-dependencies.jar -u https://www.setcce.com
-#java -jar $PROG_DIR/httpsTest-1.0.0-jar-with-dependencies.jar -u https://www.setcce.com > httpsTest.log 2>&1
+java -jar $PROG_DIR/httpsTest-1.0.0.jar -u https://www.setcce.com
+#java -jar $PROG_DIR/httpsTest-1.0.0.jar -u https://www.setcce.com > httpsTest.log 2>&1
 
-java -Djavax.net.debug=ssl,handshake -jar $PROG_DIR/httpsTest-1.0.0-jar-with-dependencies.jar -u https://www.setcce.com
+java -Djavax.net.debug=ssl,handshake -jar $PROG_DIR/httpsTest-1.0.0.jar -u https://www.setcce.com
 
-#java -Djavax.net.debug=ssl:handshake:verbose:keymanager:trustmanager -Djava.security.debug=access:stack -jar $PROG_DIR/httpsTest-1.0.0-jar-with-dependencies.jar -u https://www.setcce.com
+#java -Djavax.net.debug=ssl:handshake:verbose:keymanager:trustmanager -Djava.security.debug=access:stack -jar $PROG_DIR/httpsTest-1.0.0.jar -u https://www.setcce.com
+
+#java -Djavax.net.debug=all \
+#  -Djavax.net.ssl.keyStore=$HOME/devel/project/017_nijz/nijz.vs.11/svn_nijz.vs2/trunk/src/VS.postarCaScheduler/zzzs/zzzs.keystore.jks \
+#  -Djavax.net.ssl.keyStorePassword=VSpass_11_22 \
+#  -Djavax.net.ssl.trustStoreType=jks \
+#  -Djavax.net.ssl.trustStore=$HOME/devel/project/017_nijz/nijz.vs.11/svn_nijz.vs2/trunk/src/VS.postarCaScheduler/zzzs/cacerts.jks \
+#  -Djavax.net.ssl.trustStorePassword=VSpass_11_22 \
+#  -jar httpsTest-1.0.0.jar -u https://localhost:11443/NOZ4_Web/services/zzzsPaketiServicePort
 
 echo That_s it.
 
